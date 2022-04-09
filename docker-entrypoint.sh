3f9ed092ab8f40178a077acdd3254935f9323a33
kamino <kamino@imea.me>
2022-04-09 17:25:52 +0800

    debug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /root/app
 nohup /usr/bin/Xvfb :99 -ac -screen 0 1280x960x24 > Xvfb.log &
-nohup /usr/bin/msedgedriver > msedgedriver.log &
+nohup /usr/bin/msedgedriver --verbose > msedgedriver.log &
 python3 download.py
