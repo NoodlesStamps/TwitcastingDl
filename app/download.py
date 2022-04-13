ad0cb9c2bfbf3025977f88689fd01e53797d51c0
kamino <kamino@imea.me>
2022-04-13 23:18:02 +0800

    test

--- app/download.py
+++ app/download.py
@@ -149,7 +149,7 @@ class TwitcastingDl:
             output = "%s_%s_%s" % (self.user_id, self.video_id, code)
             logging.info("[%s]start download video stream" % code)
             download_process = subprocess.Popen(['minyami', '-d', '%s' % media_url, '--output', '%s.ts' % output, '--headers', 'Referer: https://twitcasting.tv/',
-                            '--headers', 'User-Agent: %s' % self.ua, '--threads 3'], stdout=subprocess.PIPE)
+                                                 '--headers', 'User-Agent: %s' % self.ua, '--threads 3'], stdout=subprocess.PIPE)
             while download_process.poll() is None:
                 buffer = download_process.stdout.read(200)
                 print(buffer.decode('utf8'))
@@ -219,5 +219,5 @@ class TwitcastingDl:
 
 if __name__ == "__main__":
     tc = TwitcastingDl(
-        os.getenv("TWITCASTING_URL"), os.getenv("onedrive_token_key"))
+        os.getenv("TWITCASTING_URL"), os.getenv("ONEDRIVE_TOKEN_URL"), os.getenv("ONEDRIVE_TOKEN_KEY"))
     tc.run()
